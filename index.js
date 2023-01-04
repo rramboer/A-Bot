b759f8606812297cb2387f3d0e04c2e8f49ec5e3
rramboer <ryanramboer@gmail.com>
2023-01-04 01:12:41 -0500

    functional bot

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
     console.log(`Ready! Logged in as ${c.user.tag}`);
+    c.guilds.fetch('927674403825790976').then(g => { //g is a guild
+        g.channels.fetch('1053546509288870008').then(ch => { //ch is a channel
+            ch.send(`<:secretemote:1054767344108437595>`);
+        })
+    })
 });
 
 // Log in to Discord with your client's token

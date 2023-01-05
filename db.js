d768049ab3d540529db87f17bdf96959c60bc3e3
GenericallyNamed <amshand@umich.edu>
2023-01-05 16:49:16 -0500

    hopefully it can access the MONGODB_URI now

--- db.js
+++ db.js
@@ -4,10 +4,12 @@ const { dotenv } = require('dotenv');
 // export const MONGODB_URI = process.env.MONGODB_URI; // retrieves connection string uri from environment variables 😁
 // export const mongoClient = new MongoClient(MONGODB_URI); // stores our mongoClient as the name very heavily implies 😏
 
+const MONGODB_URI = process.env.MONGODB_URI;
+const mongoClient = new MongoClient(MONGODB_URI);
 
 module.exports = {
-    MONGODB_URI: process.env.MONGODB_URI,
-    mongoClient: new MongoClient(MONGODB_URI),
+    MONGODB_URI: MONGODB_URI,
+    mongoClient: mongoClient,
     fetchTest: async () => {
         console.log("test");               
         await listDatabases(mongoClient);

94c0f3e82f6ebcbe98c503caf29f33d368858b93
GenericallyNamed <amshand@umich.edu>
2023-01-05 16:44:02 -0500

    fixed? maybe? 😨

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
 const { MongoDB } = require('mongodb')
 const { dotenv } = require('dotenv');
 
-export const MONGODB_URI = process.env.MONGODB_URI; // retrieves connection string uri from environment variables 😁
-export const mongoClient = new MongoClient(MONGODB_URI); // stores our mongoClient as the name very heavily implies 😏
+// export const MONGODB_URI = process.env.MONGODB_URI; // retrieves connection string uri from environment variables 😁
+// export const mongoClient = new MongoClient(MONGODB_URI); // stores our mongoClient as the name very heavily implies 😏
 
 
 module.exports = {
+    MONGODB_URI: process.env.MONGODB_URI,
+    mongoClient: new MongoClient(MONGODB_URI),
     fetchTest: async () => {
         console.log("test");               
         await listDatabases(mongoClient);

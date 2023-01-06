6720faa672ae794e633d5287411126b4cb43cf54
rramboer <ryanramboer@gmail.com>
2023-01-05 20:38:27 -0500

    minor changes

--- commands/joinCasino.js
+++ commands/joinCasino.js
@@ -6,7 +6,7 @@ module.exports = {
     description: "Join the bot casino! You can remove yourself laterwith /removeCasino.",
     // Create a legacy and slash command
     type: CommandType.BOTH,
-    callback: async ({user}) => {
+    callback: async ({ user }) => {
         // databasesList = await mongoClient.db().admin().listDatabases();
         let db = await mongoClient.db('botCasino');
         console.log(db);
@@ -16,23 +16,23 @@ module.exports = {
         console.log(user);
         console.log(user.id);
         await db.collection('users').insertOne({
-            user_id:user.id,
-            name:user.username,
-            cash:0,
-            employed:false,
-            income:0,
-            lastDaily:new Date(0).toISOString(),
-            bonusAvailable:true,
+            user_id: user.id,
+            name: user.username,
+            cash: 0,
+            employed: false,
+            income: 0,
+            lastDaily: new Date(0).toISOString(),
+            bonusAvailable: true,
         }, (error) => {
-                if(error) {
-                    console.error(error);
-                } else {
-                    console.log("Completed insertion")
-                }
+            if (error) {
+                console.error(error);
+            } else {
+                console.log("Completed insertion")
             }
+        }
         );
         return {
-            content: "Welcome to the casino! You have nothing to your name. Claim a starting bonus with /bonus, and start earning an income with /work. I love addiction 😀! Have fun! 🥳",
+            content: "Welcome to the casino! You have nothing to your name. Claim a starting bonus with `/bonus`, and start earning an income with `/work`. Try not to get addicted 😀! Have fun! 🥳",
         }
     }
 }

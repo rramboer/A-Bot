a9df0f799f5829fd6920849299f009d59943500a
GenericallyNamed <statefan0826@gmail.com>
2023-01-07 03:18:10 -0500

    try fix rps, have correct calc but mongodb not updating

--- events/interactionCreate/rps.js
+++ events/interactionCreate/rps.js
@@ -9,19 +9,17 @@ function getRandomSelection() {
 module.exports = async (interaction, user) => {
     //console.log("Event triggered");
     if (interaction.isButton()) {
-        //console.log("Interaction event triggered. " + "custom_id==" + interaction.customId + ", user==" + interaction.user + ", betAmt==" + interaction.betAmount);
         let db = await mongoClient.db('botCasino');
         let _user = await db.collection('users').findOne(
-            {
-                user_id: user.id,
-            }
+            { user_id: user.id, }
         );
         let opp = getRandomSelection();
         await interaction.deferUpdate();
         await wait(10);
         let playType = interaction.customId.substring(0, interaction.customId.indexOf(":")),
-            betAmount = interaction.customId.substring(interaction.customId.indexOf(":") + 1, interaction.customId.length);
-        if (interaction.customId.type == "rock") {
+            betAmount = parseInt(interaction.customId.substring(interaction.customId.indexOf(":") + 1, interaction.customId.length));
+        console.log("Interaction event triggered. " + "playType==" + playType + ", user==" + interaction.user + ", betAmt==" + betAmount);
+        if (playType == "rock") {
             await interaction.editReply({ content: 'Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!\nYou chose 🪨!\n', components: [] });
             await wait(2000);
             await interaction.editReply({ content: 'Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!\nYou chose 🪨!\nYour opponent chose ' + opp + "!", components: [] });
@@ -30,36 +28,36 @@ module.exports = async (interaction, user) => {
             if (opp == "🪨") {
                 await interaction.editReply({ content: 'Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!\nYou chose 🪨!\nYour opponent chose ' + opp + "!" + '\nThat\'s a tie! You get your bet back.', components: [] });
             } else if (opp == "📃") {
-                await interaction.editReply({ content: 'Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!\nYou chose 🪨!\nYour opponent chose ' + opp + "!" + '\nYou win your bet of ' + interaction.betAmount + '. 🥳💃🎉', components: [] });
+                await interaction.editReply({ content: 'Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!\nYou chose 🪨!\nYour opponent chose ' + opp + "!" + '\nYou win your bet of ' + betAmount + '. 🥳💃🎉', components: [] });
                 db.collection('users').updateOne({ user_id: user.id }, { $inc: { coins: betAmount } });
             } else if (opp == "✂️") {
-                await interaction.editReply({ content: 'Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!\nYou chose 🪨!\nYour opponent chose ' + opp + "!" + '\nSorry, you lose ' + interaction.betAmount + '. Better luck next time! 🤦😬', components: [] });
-                db.collection('users').updateOne({ user_id: user.id }, { $inc: { coins: -1 * betAmount } });
+                await interaction.editReply({ content: 'Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!\nYou chose 🪨!\nYour opponent chose ' + opp + "!" + '\nSorry, you lose ' + betAmount + '. Better luck next time! 🤦😬', components: [] });
+                db.collection('users').updateOne({ user_id: user.id }, { $dec: { coins: betAmount } });
             }
-        } else if (interaction.customId.type == "paper") {
+        } else if (playType == "paper") {
             await interaction.editReply({ content: 'Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!\nYou chose 📃!\n', components: [] });
             await wait(2000);
             await interaction.editReply({ content: 'Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!\nYou chose 📃!\nYour opponent chose ' + opp, components: [] });
             await wait(1000);
             if (opp == "🪨") {
-                await interaction.editReply({ content: 'Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!\nYou chose 📃!\nYour opponent chose ' + opp + "!" + '\nYou win your bet of ' + interaction.betAmount + '. 🥳💃🎉', components: [] });
+                await interaction.editReply({ content: 'Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!\nYou chose 📃!\nYour opponent chose ' + opp + "!" + '\nYou win your bet of ' + betAmount + '. 🥳💃🎉', components: [] });
                 db.collection('users').updateOne({ user_id: user.id }, { $inc: { coins: betAmount } });
             } else if (opp == "📃") {
                 await interaction.editReply({ content: 'Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!\nYou chose 📃!\nYour opponent chose ' + opp + "!" + '\nThat\'s a tie! You get your bet back.', components: [] });
             } else if (opp == "✂️") {
-                await interaction.editReply({ content: 'Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!\nYou chose 📃!\nYour opponent chose ' + opp + "!" + '\nSorry, you lose ' + interaction.betAmount + '. Better luck next time! 🤦😬', components: [] });
-                db.collection('users').updateOne({ user_id: user.id }, { $inc: { coins: -1 * betAmount } });
+                await interaction.editReply({ content: 'Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!\nYou chose 📃!\nYour opponent chose ' + opp + "!" + '\nSorry, you lose ' + betAmount + '. Better luck next time! 🤦😬', components: [] });
+                db.collection('users').updateOne({ user_id: user.id }, { $dec: { coins: betAmount } });
             }
-        } else if (interaction.customId.type == "scissors") {
+        } else if (playType == "scissors") {
             await interaction.editReply({ content: 'Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!\nYou chose ✂️!\n', components: [] });
             await wait(2000);
             await interaction.editReply({ content: 'Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!\nYou chose ✂️!\nYour opponent chose ' + opp + "!", components: [] });
             await wait(1000);
             if (opp == "🪨") {
-                await interaction.editReply({ content: 'Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!\nYou chose ✂️!\nYour opponent chose ' + opp + "!" + '\nSorry, you lose ' + interaction.betAmount + '. Better luck next time! 🤦😬', components: [] });
-                db.collection('users').updateOne({ user_id: user.id }, { $inc: { coins: -1 * betAmount } });
+                await interaction.editReply({ content: 'Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!\nYou chose ✂️!\nYour opponent chose ' + opp + "!" + '\nSorry, you lose ' + betAmount + '. Better luck next time! 🤦😬', components: [] });
+                db.collection('users').updateOne({ user_id: user.id }, { $dec: { coins: betAmount } });
             } else if (opp == "📃") {
-                await interaction.editReply({ content: 'Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!\nYou chose ✂️!\nYour opponent chose ' + opp + "!" + '\nYou win your bet of ' + interaction.betAmount + '. 🥳💃🎉', components: [] });
+                await interaction.editReply({ content: 'Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!\nYou chose ✂️!\nYour opponent chose ' + opp + "!" + '\nYou win your bet of ' + betAmount + '. 🥳💃🎉', components: [] });
                 db.collection('users').updateOne({ user_id: user.id }, { $inc: { coins: betAmount } });
             } else if (opp == "✂️") {
                 await interaction.editReply({ content: 'Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!\nYou chose ✂️!\nYour opponent chose ' + opp + "!" + '\nThat\'s a tie! You get your bet back.', components: [] });

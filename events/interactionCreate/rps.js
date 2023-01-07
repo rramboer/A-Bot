708496b2397272114a79d8e60bfb9b4209f15eb7
Ryan Ramboer <ryanramboer@gmail.com>
2023-01-07 17:04:08 -0500

    rps fix

--- events/interactionCreate/rps.js
+++ events/interactionCreate/rps.js
@@ -15,55 +15,55 @@ module.exports = async (interaction, user) => {
         await interaction.deferUpdate();
         await wait(100);
         let customId = interaction.customId;
-        console.log("BUTTON INTERACTION customId = " + customId);
+        //console.log("BUTTON INTERACTION customId = " + customId);
         let first_colon = customId.indexOf(":"),
-        second_colon = customId.indexOf(":",first_colon+1);
+            second_colon = customId.indexOf(":", first_colon + 1);
         let playType = customId.substring(0, customId.indexOf(":")),
             betAmount = parseInt(customId.substring(first_colon + 1, second_colon)),
-            verification_id = customId.substring(second_colon+1,customId.length);
-        console.log("Interaction trigged by user with ID " + interaction.user.id + ". VERIFICATION_ID is " + verification_id);
-        if(verification_id != _user.user_id) { return; }
-        console.log("Interaction event triggered. " + "playType==" + playType + ", user==" + interaction.user + ", betAmt==" + betAmount);
+            verification_id = customId.substring(second_colon + 1, customId.length);
+        //console.log("Interaction trigged by user with ID " + interaction.user.id + ". VERIFICATION_ID is " + verification_id);
+        if (verification_id != _user.user_id) { return; }
+        //console.log("Interaction event triggered. " + "playType==" + playType + ", user==" + interaction.user + ", betAmt==" + betAmount);
         var game_multiplier = 0;
-        if(playType == "rock") {
+        if (playType == "rock") {
             game_multiplier = (opp == "📃") ? -1 : ((opp == "✂️") ? 1 : 0);
-        } else if(playType == "paper") {
+        } else if (playType == "paper") {
             game_multiplier = (opp == "📃") ? 0 : ((opp == "✂️") ? -1 : 0);
-        } else if(playType == "scissors") {
+        } else if (playType == "scissors") {
             game_multiplier = (opp == "📃") ? 1 : ((opp == "✂️") ? 0 : -1);
         }
         playType = (playType == "rock") ? "🪨" : ((playType == "scissors") ? "✂️" : "📃");
-        console.log("ROSHAMBO: editing reply");
-        await interaction.editReply({ 
-            content:   (
-`Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!
+        //console.log("ROSHAMBO: editing reply");
+        await interaction.editReply({
+            content: (
+                `Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!
 You chose ${playType}!\n`),
-            components: [] 
+            components: []
         });
         let final_message = ``;
-        if(game_multiplier == 0) {
-            final_message = `🤦 A tie! ${interaction.user.username}'s get their bet back. `
-        } else if(game_multiplier == -1) {
+        if (game_multiplier == 0) {
+            final_message = `🤦 A tie! ${interaction.user.username}'s bet is returned.`
+        } else if (game_multiplier == -1) {
             final_message = `☠️ Fail! ${interaction.user.username}'s bet of ${betAmount} coins has evaporated from existence! 😵`
         } else {
-            final_message = `Victory! ${interaction.user.username} wins back their bet of ${betAmount}! 🥳💃🎉`
+            final_message = `Victory! ${interaction.user.username} wins their bet amount of ${betAmount}! 🥳💃🎉`
         }
         await wait(1000);
-        await interaction.editReply({ 
-            content:   (
-`Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!
+        await interaction.editReply({
+            content: (
+                `Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!
 You chose ${playType}
 Your opponent chose ${opp}`),
-            components: [] 
+            components: []
         });
         await wait(1000);
-        await interaction.editReply({ 
-            content:   (
-`Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!
+        await interaction.editReply({
+            content: (
+                `Time for some old-fashioned roshambo! Rock, paper, or scissors? Pick one!
 You chose ${playType}
 Your opponent chose ${opp}
 ${final_message}`),
-            components: [] 
+            components: []
         });
         db.collection('users').updateOne({ user_id: interaction.user.id }, { $inc: { coins: game_multiplier * betAmount } });
         // if (playType == "rock") {

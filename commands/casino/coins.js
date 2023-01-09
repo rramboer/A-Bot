8ffb279f652436f7426c65f47032044435b03bc9
rramboer <ryanramboer@gmail.com>
2023-01-09 16:57:53 -0500

    coins fix

--- commands/casino/coins.js
+++ commands/casino/coins.js
@@ -12,28 +12,30 @@ module.exports = {
     callback: async ({ client, user, args }) => {
         try {
             let db = await mongoClient.db('botCasino');
-            let _user = await db.collection('users').findOne(
-                {
-                    user_id: (args.length == 1) ? args[0] : user.id,
-                }
-            );
-            if (args.length == 1) { console.log(`COINS command: User ID argument passed in, given user id ${args[0]}.`); }
             var user_id;
             var user_id_input = args[0];
-            if(args.length == 1) {
-                if(user_id_input[0] == "<" && user_id_input[1] == "@") {
+            if (args.length == 1) {
+                if (user_id_input[0] == "<" && user_id_input[1] == "@") {
                     user_id_input = user_id_input.substring(2, user_id_input.length - 1);
                 }
+            }
+            let _user = await db.collection('users').findOne(
+                {
+                    user_id: (args.length == 1) ? user_id_input : user.id,
+                }
+            );
+            //if (args.length == 1) { console.log(`COINS command: User ID argument passed in, given user id ${args[0]}.`); }
+            if (args.length == 1) {
                 user_id = await client.users.fetch(user_id_input).catch((e) => console.error(e));
             } else {
                 user_id = await client.users.fetch(user.id).catch(() => null);
             }
-                
+
             // const user_id = await client.users.fetch((args.length == 1) ? args[0] : user.id).catch(() => null);
             if (!user_id) console.log("That user is not available");
             if (_user == undefined || _user == null || _user == NaN) {
-                if(args.length > 0) {
-                    return { content: `I searched high and low and couldn't find that user. Do they even exist?`}
+                if (args.length > 0) {
+                    return { content: `I searched high and low and couldn't find that user. Do they even exist?` }
                 }
                 return {
                     content: "To play, you need to join the casino first. Do so by running the `/joincasino` command!"

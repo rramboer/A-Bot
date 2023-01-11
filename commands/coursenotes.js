71d9c35b6948fd6e343fa6c7f8ce82bcaaec2e27
rramboer <ryanramboer@gmail.com>
2023-01-10 22:58:56 -0500

    v1.9.5

--- commands/coursenotes.js
+++ commands/coursenotes.js
@@ -6,12 +6,6 @@ module.exports = {
     // Create a legacy and slash command
     type: CommandType.BOTH,
     callback: async ({ user }) => {
-        // Return the same object you would use in
-        // message.reply
-        // or
-        // interaction.reply
-        // WOKCommands will reply to the message or the interaction
-        // depending on how the user ran the command (legacy vs slash)
         console.log(`User ${user.username} requested a link to the course notes.`);
         return {
             content: "Check out the (unofficial) course notes at https://github.com/danlliu/eecs370notes !",

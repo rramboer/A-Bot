faa932571864b0fa56a2caf8650cdcd10695a5a9
rramboer <ryanramboer@gmail.com>
2023-01-10 23:48:42 -0500

    sourcecode

--- commands/sourcecode.js
+++ commands/sourcecode.js
@@ -2,13 +2,13 @@ const { CommandType } = require("wokcommands");
 
 module.exports = {
     // Required for slash commands
-    description: "Get a link to the course notes.",
+    description: "Posts a link to the source code for A-Bot",
     // Create a legacy and slash command
     type: CommandType.BOTH,
     callback: async ({ user }) => {
-        console.log(`User ${user.username} requested a link to the course notes.`);
+        console.log(`User ${user.username} requested the A-Bot source code.`);
         return {
-            content: "The source code for A-Bot is at  <https://github.com/rramboer/A-Bot/tree/master>"
+            content: "The source code for A-Bot is at  <https://github.com/rramboer/A-Bot/tree/master>."
         }
     }
 }
